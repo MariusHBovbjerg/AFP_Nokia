521117ca481e226632e41575ff210cc5647578d9
Marius Hambro Bovbjerg <marius@hambro.eu>
2022-02-15 09:41:50 +0100

    yo

--- WaveFormWeb/Controllers/HomeController.fs
+++ WaveFormWeb/Controllers/HomeController.fs
@@ -14,18 +14,3 @@ type HomeController (logger : ILogger<HomeController>) =
 
     member this.Index () =
         this.View()
-
-   (* [<HttpPost>]
-    member this.Produce(score:string) =
-        //let cd = ContentDisposition(FileName = "sound", Inline = true)
-        match Assembler.assembleToPackedStream score with
-            | Choice1Of2 ms -> WavePacker.writeSingle "sound"
-            | Choice2Of2 err -> failwith err
-
-            | Choice1Of2 ms ->
-                this.Response.Headers.Add(
-                    "Content-Disposition",
-                    StringValues(cd.ToString()))
-                ms.Position <- 0L
-                this.File(ms, "audio/x-wav")
-            | Choice2Of2 err -> failwith err*)
