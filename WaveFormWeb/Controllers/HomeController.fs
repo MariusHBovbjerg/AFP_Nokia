3b36204fa37aed07d92bb7f166386467772c89df
Marius Hambro Bovbjerg <marius@hambro.eu>
2022-03-05 16:02:01 +0100

    usings

--- WaveFormWeb/Controllers/HomeController.fs
+++ WaveFormWeb/Controllers/HomeController.fs
@@ -1,13 +1,7 @@
 ﻿namespace WaveFormWeb.Controllers
 
-open System
-open System.Collections.Generic
-open System.Linq
-open System.Threading.Tasks
-open System.Net.Mime
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
-open Microsoft.Extensions.Primitives
 
 type HomeController (logger : ILogger<HomeController>) =
     inherit Controller()
