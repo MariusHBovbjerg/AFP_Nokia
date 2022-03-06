ca0b81f9b9bcb2760778d194665adb0206fbe755
eengstroem <59916483+eengstroem@users.noreply.github.com>
2022-03-06 15:37:22 +0100

    Styling changes, and about page added :)

---
+++ WaveFormWeb/Controllers/AboutController.fs
@@ -0,0 +1,11 @@
+﻿namespace WaveFormWeb.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+type AboutController (logger : ILogger<AboutController>) =
+    inherit Controller()
+
+    member this.About () = 
+        this.View()
+
